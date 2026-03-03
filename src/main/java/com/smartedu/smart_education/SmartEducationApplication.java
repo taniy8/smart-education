1144c4d1e73a637fa5dba59f322def66147463fe
Taniya <taniyasaxena804@gmail.com>
2026-03-03 18:38:31 +0530

    Initial commit from correct folder

---
+++ src/main/java/com/smartedu/smart_education/SmartEducationApplication.java
@@ -0,0 +1,13 @@
+package com.smartedu.smart_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEducationApplication.class, args);
+	}
+
+}
