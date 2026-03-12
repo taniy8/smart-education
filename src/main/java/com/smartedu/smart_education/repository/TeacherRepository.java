c1bc26a558111d1e18a9d8250248c3be70fa7975
Taniya <taniyasaxena804@gmail.com>
2026-03-12 21:29:49 +0530

    TeacherService Implementation

--- src/main/java/com/smartedu/smart_education/repository/TeacherRepository.java
+++ src/main/java/com/smartedu/smart_education/repository/TeacherRepository.java
@@ -4,6 +4,12 @@ import com.smartedu.smart_education.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher,Long> {
+    Boolean existsByEmployeeCode(String employeeCode);
+    Optional<Teacher> findByEmployeeCode(String employeeCode);
+   List<Teacher> findByDepartment(String department);
 }
--- src/main/java/com/smartedu/smart_education/service/TeacherService.java
+++ src/main/java/com/smartedu/smart_education/service/TeacherService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface TeacherService {
     Teacher addTeacher(Teacher teacher);
-    Teacher updateTeacher(Teacher teacher);
+    Teacher updateTeacher(Long id,Teacher updatedTeacher);
     void deleteTeacher(Long id);
     Teacher getTeacherById(Long id);
     List<Teacher> getAllTeacherByDepartment(String department);
