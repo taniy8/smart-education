1f4c7f23c7b39792f810188274334eef9b33e02f
Taniya <taniyasaxena804@gmail.com>
2026-03-14 19:38:01 +0530

    Attendance Implementation

--- src/main/java/com/smartedu/smart_education/service/AttendanceService.java
+++ src/main/java/com/smartedu/smart_education/service/AttendanceService.java
@@ -7,14 +7,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface AttendanceService {
-    List<Attendance> getAttendanceBySubject(String subject);
-    List<Attendance> getAttendanceByStudent(Student student);
-    List<Attendance> getAttendanceByStatus(String status);
-    List<Attendance> getAttendanceByClass(String classCode);
+    List<Attendance> getAttendanceByStudent(Long studentId);
     Attendance markAttendance(Attendance attendance);
     List<Attendance> getAttendanceBetweenDates(Long studentId, LocalDate startDate, LocalDate endDate);
     Double getAttendancePercentage(Long studentId);
     Long getTotalPresentDays(Long studentId);
-    Attendance updateAttendance(Long id, Attendance attendance);
+    Attendance updateAttendance(Long id, Attendance updatedAttendance);
 
 }
