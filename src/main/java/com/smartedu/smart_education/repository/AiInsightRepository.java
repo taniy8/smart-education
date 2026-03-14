88e913032fcee242474c90008654103547861166
Taniya <taniyasaxena804@gmail.com>
2026-03-14 19:03:37 +0530

    AiInsightService Implementation

--- src/main/java/com/smartedu/smart_education/repository/AiInsightRepository.java
+++ src/main/java/com/smartedu/smart_education/repository/AiInsightRepository.java
@@ -4,6 +4,12 @@ import com.smartedu.smart_education.entity.AiInsight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AiInsightRepository extends JpaRepository<AiInsight,Long> {
+    List<AiInsight> findByStudentId(Long studentId);
+
+   Optional<AiInsight> findTopByStudentIdOrderByGeneratedOnDesc(Long studentId);
 }
