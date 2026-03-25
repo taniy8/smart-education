742819e2c8eab47e205207a7b27dc6dad901653e
Taniya <taniyasaxena804@gmail.com>
2026-03-25 21:40:59 +0530

    Exception Added

--- src/main/java/com/smartedu/smart_education/service/impl/AttendanceServiceImpl.java
+++ src/main/java/com/smartedu/smart_education/service/impl/AttendanceServiceImpl.java
@@ -2,6 +2,7 @@ package com.smartedu.smart_education.service.impl;
 
 import com.smartedu.smart_education.entity.Attendance;
 import com.smartedu.smart_education.entity.Student;
+import com.smartedu.smart_education.exception.ResourceNotFoundException;
 import com.smartedu.smart_education.repository.AttendanceRepository;
 import com.smartedu.smart_education.repository.StudentRepository;
 import com.smartedu.smart_education.repository.SubjectRepository;
@@ -34,7 +35,7 @@ public class AttendanceServiceImpl implements AttendanceService {
     @Override
     public Attendance markAttendance(Attendance attendance) {
         studentRepo.findById(attendance.getStudent().getId())
-                .orElseThrow(() -> new RuntimeException("Student not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found"));
         if (attendance.getDate() == null) {
             attendance.setDate(LocalDate.now());
         }
@@ -61,7 +62,7 @@ public class AttendanceServiceImpl implements AttendanceService {
     @Override
     public Attendance updateAttendance(Long id, Attendance updatedAttendance) {
         Attendance existing = attendanceRepo.findById(id)
-                .orElseThrow(() -> new RuntimeException("Attendance not found with id: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Attendance not found with id: " + id));
         existing.setStatus(updatedAttendance.getStatus());
         return attendanceRepo.save(existing);
     }
