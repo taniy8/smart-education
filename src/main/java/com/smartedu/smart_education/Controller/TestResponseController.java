aedab67ea90b5b212dc9c6014c24c39749e25054
Taniya <taniyasaxena804@gmail.com>
2026-04-10 23:17:12 +0530

    feat: Implement dto functionality in TestResponse

--- src/main/java/com/smartedu/smart_education/Controller/TestResponseController.java
+++ src/main/java/com/smartedu/smart_education/Controller/TestResponseController.java
@@ -1,38 +1,37 @@
 package com.smartedu.smart_education.Controller;
 
-import com.smartedu.smart_education.entity.TestResponse;
+import com.smartedu.smart_education.dto.response.TestResponseDto;
 import com.smartedu.smart_education.service.TestResponseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/test-responses")
+@RequestMapping("/api/test-responses")
+@RequiredArgsConstructor
 public class TestResponseController {
+
     private final TestResponseService testResponseService;
 
-    public TestResponseController(TestResponseService testResponseService) {
-        this.testResponseService = testResponseService;
-    }
-
     @PostMapping
-    public ResponseEntity<TestResponse> submitResponse(@RequestBody TestResponse testResponse) {
+    public ResponseEntity<TestResponseDto> submitResponse(@RequestBody com.smartedu.smart_education.entity.TestResponse testResponse) {
         return ResponseEntity.status(201).body(testResponseService.submitResponse(testResponse));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TestResponse> getResponseById(@PathVariable Long id) {
+    public ResponseEntity<TestResponseDto> getResponseById(@PathVariable Long id) {
         return ResponseEntity.ok(testResponseService.getResponseById(id));
     }
 
     @GetMapping("/student/{studentId}")
-    public ResponseEntity<List<TestResponse>> getResponsesByStudent(@PathVariable Long studentId) {
+    public ResponseEntity<List<TestResponseDto>> getResponsesByStudent(@PathVariable Long studentId) {
         return ResponseEntity.ok(testResponseService.getResponsesByStudent(studentId));
     }
 
     @GetMapping("/test/{testId}")
-    public ResponseEntity<List<TestResponse>> getResponsesByTest(@PathVariable Long testId) {
+    public ResponseEntity<List<TestResponseDto>> getResponsesByTest(@PathVariable Long testId) {
         return ResponseEntity.ok(testResponseService.getResponsesByTest(testId));
     }
 }
