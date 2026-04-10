4be3d5bd27aaab396c32dc80f6bb1972d4b849cc
Taniya <taniyasaxena804@gmail.com>
2026-04-10 23:17:31 +0530

    feat: Implement dto functionality in TestResponseService

--- src/main/java/com/smartedu/smart_education/service/TestResponseService.java
+++ src/main/java/com/smartedu/smart_education/service/TestResponseService.java
@@ -1,12 +1,13 @@
 package com.smartedu.smart_education.service;
 
+import com.smartedu.smart_education.dto.response.TestResponseDto;
 import com.smartedu.smart_education.entity.TestResponse;
 
 import java.util.List;
 
 public interface TestResponseService {
-    TestResponse submitResponse(TestResponse testResponse);
-    TestResponse getResponseById(Long id);
-    List<TestResponse> getResponsesByStudent(Long studentId);
-    List<TestResponse> getResponsesByTest(Long testId);
+    TestResponseDto submitResponse(TestResponse testResponse);
+    TestResponseDto getResponseById(Long id);
+    List<TestResponseDto> getResponsesByStudent(Long studentId);
+    List<TestResponseDto> getResponsesByTest(Long testId);
 }
