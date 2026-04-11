1b6673a3f1d3b2b8364fda871dfae816870a65f6
Taniya <taniyasaxena804@gmail.com>
2026-04-11 22:19:34 +0530

    feat: add new AiExceptionMethod with id

--- src/main/java/com/smartedu/smart_education/exception/AiServiceException.java
+++ src/main/java/com/smartedu/smart_education/exception/AiServiceException.java
@@ -4,6 +4,9 @@ public class AiServiceException extends RuntimeException {
     public AiServiceException(String message) {
         super(message);
     }
+    public AiServiceException(String resourceName, Long id){
+        super(resourceName + " not found with id: " + id);
+    }
 
 }
 
